eb3ebd0b0dfa129c2ccf71cacc87dad11b8822db
Faisal Abid <ifaisalabid1@gmail.com>
2025-10-30 22:06:00 +0530

    Add lawyer and law firm models

---
+++ Models/LawFirm.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ConnectLawyers.Models;
+
+public class LawFirm
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "About is required.")]
+    [MaxLength(500, ErrorMessage = "Name must not be more than 500 characters long.")]
+    public required string About { get; set; }
+
+    [Url(ErrorMessage = "Website must be a proper url.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public string? Website { get; set; }
+
+    [Phone(ErrorMessage = "Phone number is invalid.")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    [EmailAddress(ErrorMessage = "Email address is invalid.")]
+    public required string Email { get; set; }
+    public bool IsFeatured { get; set; } = false;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<Lawyer> Lawyers { get; set; } = [];
+}
