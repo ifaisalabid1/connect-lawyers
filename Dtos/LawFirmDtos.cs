7318568e3d0dbdcf95e037049099f1c793681ece
Faisal Abid <ifaisalabid1@gmail.com>
2025-10-31 11:21:52 +0530

    Add dbcontext and dtos

---
+++ Dtos/LawFirmDtos.cs
@@ -0,0 +1,86 @@
+using System.ComponentModel.DataAnnotations;
+namespace ConnectLawyers.Dtos;
+
+public class LawFirmDto
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "About is required.")]
+    [MaxLength(500, ErrorMessage = "Name must not be more than 500 characters long.")]
+    public required string About { get; set; }
+
+    [Url(ErrorMessage = "Website must be a proper url.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public string? Website { get; set; }
+
+    [Phone(ErrorMessage = "Phone number is invalid.")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email address is invalid.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Email { get; set; }
+    public bool IsFeatured { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+}
+
+public class LawFirmCreateDto
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "About is required.")]
+    [MaxLength(500, ErrorMessage = "Name must not be more than 500 characters long.")]
+    public required string About { get; set; }
+
+    [Url(ErrorMessage = "Website must be a proper url.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public string? Website { get; set; }
+
+    [Phone(ErrorMessage = "Phone number is invalid.")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email address is invalid.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Email { get; set; }
+    public bool IsFeatured { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
+
+public class LawFirmUpdateDto
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "About is required.")]
+    [MaxLength(500, ErrorMessage = "Name must not be more than 500 characters long.")]
+    public required string About { get; set; }
+
+    [Url(ErrorMessage = "Website must be a proper url.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public string? Website { get; set; }
+
+    [Phone(ErrorMessage = "Phone number is invalid.")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email address is invalid.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Email { get; set; }
+    public bool IsFeatured { get; set; } = false;
+    public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+}
+
+public class LawFirmDetailDto : LawFirmDto
+{
+    public List<LawyerDto> Lawyers { get; set; } = [];
+}
---
+++ Dtos/LawyerDtos.cs
@@ -0,0 +1,97 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ConnectLawyers.Dtos;
+
+public class LawyerDto
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "About is required.")]
+    [MaxLength(500, ErrorMessage = "Name must not be more than 500 characters long.")]
+    public required string About { get; set; }
+
+    [Url(ErrorMessage = "Website must be a proper url.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public string? Website { get; set; }
+
+    [Phone(ErrorMessage = "Phone number is invalid.")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email address is invalid.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Email { get; set; }
+    public bool IsFeatured { get; set; } = false;
+
+    [Range(0, 100, ErrorMessage = "Year of Experience must be between 0 and 100.")]
+    public int YearOfExperience { get; set; } = 0;
+    public int? LawFirmId { get; set; }
+    public string? LawFirmName { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+}
+
+public class LawyerCreateDto
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "About is required.")]
+    [MaxLength(500, ErrorMessage = "Name must not be more than 500 characters long.")]
+    public required string About { get; set; }
+
+    [Url(ErrorMessage = "Website must be a proper url.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public string? Website { get; set; }
+
+    [Phone(ErrorMessage = "Phone number is invalid.")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email address is invalid.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Email { get; set; }
+    public bool IsFeatured { get; set; } = false;
+
+    public int? LawFirmId { get; set; }
+
+    [Range(0, 100, ErrorMessage = "Year of Experience must be between 0 and 100.")]
+    public int YearOfExperience { get; set; } = 0;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+}
+
+public class LawyerUpdateDto
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Name { get; set; }
+
+    [Required(ErrorMessage = "About is required.")]
+    [MaxLength(500, ErrorMessage = "Name must not be more than 500 characters long.")]
+    public required string About { get; set; }
+
+    [Url(ErrorMessage = "Website must be a proper url.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public string? Website { get; set; }
+
+    [Phone(ErrorMessage = "Phone number is invalid.")]
+    public string? Phone { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email address is invalid.")]
+    [MaxLength(255, ErrorMessage = "Name must not be more than 255 characters long.")]
+    public required string Email { get; set; }
+    public bool IsFeatured { get; set; } = false;
+
+    [Range(0, 100, ErrorMessage = "Year of Experience must be between 0 and 100.")]
+    public int YearOfExperience { get; set; } = 0;
+
+    public int? LawFirmId { get; set; }
+    public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+}
